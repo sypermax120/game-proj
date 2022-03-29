a02fa5b342e987dee27975247dd59afbe8661c54
Maks <sypermax120@gmail.com>
2022-03-29 17:31:49 +0200

    add footer and nav bar

--- script/script.js
+++ script/script.js
@@ -3,12 +3,18 @@ let countNav = 0;
 $( ".header-menu-dots" ).click(function() {
     if(countNav%2==0){
         // $( "header" ).addClass('header-nav-menu');
-        $( ".header-menu" ).addClass('header-nav-menu');
+        // $( ".header-menu" ).addClass('header-menu-active');
+        // $(".inner-header-menu").addClass('inner-header-menu-active');
+        $(".inner-header-menu").css({"display": "flex"});
+
         
 
     }else{
         // $("header" ).removeClass("header-nav-menu");
-        $( ".header-menu" ).removeClass('header-nav-menu');
+        // $( ".header-menu" ).removeClass('header-menu-active');
+        // $(".inner-header-menu").removeClass('inner-header-menu-active');
+        $(".inner-header-menu").css({"display": "none"});
+
     }
     countNav+=1;
 
@@ -19,21 +25,28 @@ $( ".header-menu-dots" ).click(function() {
 
     if(window.innerWidth>1200){
         // $("header" ).removeClass("header-nav-menu");
-        $( ".header-menu" ).removeClass('header-nav-menu');
+        // $( ".header-menu" ).removeClass('header-nav-menu');
+        // $( ".header-menu" ).removeClass('header-menu-active');
+        // $(".inner-header-menu").removeClass('inner-header-menu-active');
+        $(".inner-header-menu").css({"display": "flex"});
         countNav = 0
+    }else{
+        $(".inner-header-menu").css({"display": "none"});
     }
-//     if(window.innerWidth>300){
-//         $( ".gameBox-img img" ).css({"width": `${250}`});
-//     }
-//     if(window.innerWidth<300){
-//         $( ".gameBox-img img" ).css({"width": `${window.innerWidth-50}`});
-//     }
-//     if(window.innerWidth<450){
-//         $( ".blog-games img" ).css({"width": `${window.innerWidth-50}`});
-//     }
-//     if(window.innerWidth>450){
-//         $( ".blog-games img" ).css({"width": `${400}`});
-//     }
+    // if(window.innerWidth>300){
+    //     $( ".gameBox-img img" ).css({"width": `${250}`});
+    // }
+    // if(window.innerWidth<300){
+    //     $( ".gameBox-img img" ).css({"width": `${window.innerWidth-50}`});
+    // }
+    // if(window.innerWidth<450){
+    //     $( ".blog-games img" ).css({"width": `${window.innerWidth-50}`});
+    // }
+    // if(window.innerWidth>450){
+    //     $( ".blog-games img" ).css({"width": `${400}`});
+    // }
     
     // console.log(window.innerWidth)
-  }
+}
+
+
